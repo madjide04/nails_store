a17dff25eb34cfc13a8bbdf642a712fbf9af2d50
madjide04 <madjideibdaien@gmail.com>
2024-11-09 00:11:22 +0100

    added the files to the repository

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
 import Hero from "../components/hero/Hero"
 import MostOrdered from '../components/most_oredred/MostOrdered'
 import Overveow from '../components/overveow/Overveow'
+import Tuto from '../components/tuto/Tuto'
 
 const Main = () => {
   return (
     <>
     <Hero/>
     <MostOrdered/>
-    <Overveow/>
+      <Overveow />
+      <Tuto/>
     </>
   )
 }
